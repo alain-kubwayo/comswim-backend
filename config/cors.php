406f6963ca8ee659b455960c50d45b57e0f5dec6
Alain Kubwayo <ahkubwayo@gmail.com>
2024-04-01 10:40:20 +0200

    feature: add origin url

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'https://comswim-frontend.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
